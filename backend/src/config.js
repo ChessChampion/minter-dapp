8db543f6f3990c18fd7ddaa0d951588ea48cec02
Chess Champion <chessworldchampions@gmail.com>
2023-03-28 16:48:16 -0400

    changed presale

--- backend/src/config.js
+++ backend/src/config.js
@@ -68,7 +68,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2023-03-28T16:00:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2023-03-28T16:05:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2023-03-28T19:05:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 900; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x2D4eF0659682a026F3Ab5c798E0B7A6726Ad5612"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
